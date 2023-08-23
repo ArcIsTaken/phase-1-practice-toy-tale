71f125f273c5351b072051a948cf6430e3b6d089
Riley <riley.freier@gmail.com>
2023-08-23 14:47:04 -0600

    thansk to random guy named kevin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,96 @@
-let addToy = false;
+
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let addToy = false
+const toyUrl = 'http://localhost:3000/toys'
+
+
+const toyCollectionDiv = document.getElementById('toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
+
+  addBtn.addEventListener('click', () => {
     // hide & seek with the form
-    addToy = !addToy;
+    addToy = !addToy
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyForm.style.display = 'block'
+      // submit listener here
     } else {
-      toyFormContainer.style.display = "none";
+      toyForm.style.display = 'none'
     }
-  });
+  })
+  
+  
+  // add the toys to the page
+  const parseJSON = resp => resp.json()
+  
+  function putToysOnPage(toys) {
+    // const toyCollectionDiv = document.getElementById('toy-collection')
+  
+    toys.forEach(function (toy) {
+      toyCollectionDiv.innerHTML += `
+        <div class="card" data-id=${toy.id}>
+          <h2>${toy.name}</h2>
+          <img style="width: 100%" src="${toy.image}" class="toy-avatar" />
+          <p>${toy.likes} Likes</p>
+          <button class="like-btn">Like <3</button>
+        </div>
+      `
+    })
+    fetch(toyUrl)
+    .then(parseJSON)
+    .then(putToysOnPage)
+  
+  //new toy submission
+  const addToyForm = document.querySelector('.add-toy-form')
+  //const addToyForm = document.getElementsBoyClassName('add-toy-form')[0]
+  addToyForm.addEventListener('submit', function (event) {
+    // event.preventDefault();
+  
+    fetch(toyUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "name": `${event.target.name.value}`,
+        "image": `${event.target.image.value}`,
+        "likes": 0
+      })
+    })
+      .then(parseJSON)
+      .then(putToysOnPage)
+  
+    //making the request based on the input
+    //putting the info on the page by creating a new div card and putting that new toy's name, image and likes there on the page and appending it to toyCollectionDiv
+  })
+  }
+  fetch(toyUrl)
+  .then(parseJSON)
+  .then(putToysOnPage)
+
+//new toy submission
+const addToyForm = document.querySelector('.add-toy-form')
+//const addToyForm = document.getElementsBoyClassName('add-toy-form')[0]
+addToyForm.addEventListener('submit', function (event) {
+  // event.preventDefault();
+
+  fetch(toyUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "name": `${event.target.name.value}`,
+      "image": `${event.target.image.value}`,
+      "likes": 0
+    })
+  })
+    .then(parseJSON)
+    .then(putToysOnPage)
+
+  //making the request based on the input
+  //putting the info on the page by creating a new div card and putting that new toy's name, image and likes there on the page and appending it to toyCollectionDiv
+})
+
 });
